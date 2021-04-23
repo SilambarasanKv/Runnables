9cc49249884fc1510ef240d0e586036c684a34c5
SilambarasanKv <silambu.kvs12@gmail.com>
2021-04-23 10:38:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/runnables/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.runnables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var number: Int = 0
+    var handler: Handler = Handler()
+    var runnable: Runnable = Runnable {  }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun start(view: View) {
+        number = 0
+        runnable = object : Runnable {
+            override fun run() {
+                textView.text = "Time : " + number
+                number++
+                textView.text = "Time : " + number
+                handler.postDelayed(this, 1000)
+            }
+
+        }
+
+        handler.post(runnable)
+
+    }
+
+    fun reset(view: View) {
+        handler.removeCallbacks(runnable)
+        number = 0
+        textView.text = "Time : " + number
+    }
+}
